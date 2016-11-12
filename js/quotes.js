b33d566f07e38c0df03b5e9ed85e5fe71a194a23
Kirk <kirk.rohani@gmail.com>
2016-11-12 16:11:48 -0600

    Major changes to complete the "meets expectations"
    - Separated quotes into another file
    - Added functions for random background color
    - Modified html, css and js to display tags
    - fixed random number bug
    - Ensured button bicolor changes as well
    - started tracking which quotes have been displayed

---
+++ js/quotes.js
@@ -0,0 +1,49 @@
+// An array of quote objects some of which have a cituation and year
+var quotes = [
+	{	
+		quote: 'It does not matter how slowly you go as long as you do no stop.',
+		source: 'Confucious',
+		tags: [' Motivation', ' Persverance']
+	},
+	{	
+		quote: 'Life is 10% what happens to you and 90% how you react to it.',
+		source: 'Charles R. Swindoll',
+		tags: [' Life', ' Motivation', ' Emotions']
+	},
+	{	
+		quote: 'A creative man is motivated by the desire to achieve, not by the desire to beat others.',
+		source: 'Ayn Rand',
+		citation: "Famous Brainy Quotes",
+		tags: [' Motivation', ' Achievement']
+	},
+	{	
+		quote: 'No one can read with profit that which he cannot learn to read with pleasure.',
+		source: 'Thomas Hardy',
+		tags: [' Income', ' Career', ' Motivation']
+	},
+	{
+		quote: 'It is the mark of an educated mind to be able to entertain a thought without accepting it.',
+		source: 'Aristotle',
+		citation: "Quotations Page - www.quotationspage.com",
+		year: 322,
+		tags: [' Education']
+
+	},
+	{	
+		quote: 'It is impossible to live without failing at something, unless you live so cautiously that you mught as well not lived at all. In which case, you\'ve failed by default',
+		source: 'J.K. Rowling',
+		citation: "Harvard Commencement Address",
+		year: 2008,
+		tags: [' Failure', ' Motivation']
+	},
+	{
+		quote: 'You don\'t stop laughing because you grow old. You grow old because you stop laughing.',
+		source: 'Michael Pritchard',
+		tags: [' Laugher', ' Happiness']
+	},
+	{
+		quote: 'Happiness depends upon ourselves',
+		source: 'Aristotle',
+		tags: [' Happiness']
+	}
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,69 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var quotesGenerated = [];
+
+// function which generates a random number between 1 and 8 and then passes that to the quotes arrays
+// and then returns the random quote object from the quotes array
+function getRandomQuote()
+{
+	//var randomNumber = Math.floor(Math.random() * 8);
+	var aRandomNumber;
+
+
+	// Loop through and get a rnadom number until we get one that was not previously provided.
+	do
+	{
+		aRandomNumber = Math.floor(Math.random() * 8);
+
+	}  while (quotesGenerated[aRandomNumber] !== undefined && quotesGenerated[aRandomNumber] === true)
+
+	quotesGenerated[aRandomNumber] = true;
+	console.log(aRandomNumber + ' ' + quotes[aRandomNumber].quote);
+	return quotes[aRandomNumber];
+}
+
+/*
+* Functions to get random color values for the backgrounds
+*/
+function getRandomColorRGBValue()
+{
+	return Math.floor( Math.random() * 255) + 1;
+}
+
+function getRandomColor()
+{
+	return color = 'rgb(' + getRandomColorRGBValue() + ',' + getRandomColorRGBValue() + ',' + getRandomColorRGBValue() + ')';
+
+}
+
+// function which takes the random quote object and uses the properties to generate an HTML string
+// to display on the page when the user clicks on the 'show another quote' button
+// if a citation and/or year exist then the HTML string additional displays those properties
+function printQuote()
+{
+	var randomQuote = getRandomQuote();
+	
+
+	var quoteHTML = '<p class="quote">' + randomQuote.quote +  '</p>';
+	quoteHTML +=  '<p class="source"> ' + randomQuote.source;
+	
+	if ("citation" in randomQuote)
+		quoteHTML +=  '<span class="citation">' + randomQuote.citation +  '</span>';
+	
+	if ("year" in randomQuote)
+		quoteHTML +=  '<span class="year">' + randomQuote.year + '</span>  </p>';
+
+	if ("tags" in randomQuote)
+		quoteHTML +=  '<p class="tags"> Tags: ' + randomQuote.tags + '</p>';
+	
+		
+	// Set the quote the newly generated HTML based on what is available in the object
+	document.getElementById('quote-box').innerHTML = quoteHTML;
+
+	var randomColor = getRandomColor();
+	document.body.style.backgroundColor = randomColor;
+	document.getElementById('loadQuote').style.backgroundColor = randomColor;
+
+}
+
